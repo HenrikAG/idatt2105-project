cc9dba301857e1c59c715d349091660c42dc84d4
Henrik André Gaustad <hengau@live.no>
2025-03-31 01:48:13 +0200

    Add basic user class

---
+++ backend/src/main/java/ntnu/idatt2105/backend/model/User.java
@@ -0,0 +1,43 @@
+package ntnu.idatt2105.backend.model;
+
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.Table;
+
+/**
+ * Entity representing a user.
+ */
+@Entity
+@Table(name = "users")
+public class User {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private long id;
+
+    @Column(name = "username", nullable = false)
+    private String username;
+
+    @Column(name = "password", nullable = false)
+    private String password;
+
+    /**
+     * Empty constructor required for JPA.
+     */
+    public User() {
+    }
+
+    /**
+     * Constructor for User with username and password.
+     * 
+     * @param username the username of the user
+     * @param password the password of the user
+     */
+    public User(String username, String password) {
+        this.username = username;
+        this.password = password;
+    }
+}
