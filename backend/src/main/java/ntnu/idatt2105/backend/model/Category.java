5be70cf0c587e79af55d508eb9e5d44456bf9351
Henrik André Gaustad <hengau@live.no>
2025-04-01 22:10:37 +0200

    Update Category class to include items field

--- backend/src/main/java/ntnu/idatt2105/backend/model/Category.java
+++ backend/src/main/java/ntnu/idatt2105/backend/model/Category.java
@@ -1,10 +1,14 @@
 package ntnu.idatt2105.backend.model;
 
+import java.util.HashSet;
+import java.util.Set;
+
 import jakarta.persistence.Column;
 import jakarta.persistence.Entity;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
+import jakarta.persistence.OneToMany;
 import jakarta.persistence.Table;
 
 /**
@@ -21,6 +25,9 @@ public class Category {
     @Column(name = "name", nullable = false)
     private String name;
 
+    @OneToMany(mappedBy = "category")
+    private Set<Item> items;
+
     /**
      * Empty constructor required for JPA.
      */
@@ -34,5 +41,6 @@ public class Category {
      */
     public Category(String name) {
         this.name = name;
+        this.items = new HashSet<>();
     }
 }
