335351158e57ba309cbd74e251e5eb5fd245397c
Henrik André Gaustd <hengau@live.no>
2025-04-04 22:43:01 +0200

    Add registerItem method in ItemService. (Dont know why it didnt add last commit)

--- backend/src/main/java/ntnu/idatt2105/backend/service/ItemService.java
+++ backend/src/main/java/ntnu/idatt2105/backend/service/ItemService.java
@@ -1,11 +1,13 @@
 package ntnu.idatt2105.backend.service;
 
+import java.lang.foreign.Linker.Option;
 import java.util.List;
 import java.util.Optional;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
+import ntnu.idatt2105.backend.dto.ItemDTO;
 import ntnu.idatt2105.backend.model.Category;
 import ntnu.idatt2105.backend.model.Item;
 import ntnu.idatt2105.backend.repository.CategoryRepository;
@@ -35,11 +37,29 @@ public class ItemService {
     public List<Item> getItemsByCategoryName(String categoryName) {
         Optional<Category> selectedCategory = categoryRepository.findByName(categoryName);
 
-        if (!selectedCategory.isPresent()) {
+        if (selectedCategory.isEmpty()) {
             throw new IllegalArgumentException("No category with the specified name exists.");
         }
 
         List<Item> categoryItems = itemRepository.findByCategory(selectedCategory.get());
         return categoryItems;
+    }
+
+    /**
+     * Registers a new item and returns the registered item.
+     * 
+     * @param itemRequest an ItemDTO object with the item to be registered
+     * @return The registered Item object
+     * @throws IllegalArgumentException if there is no category with the name specified by the request object
+     */
+    public Item registerItem(ItemDTO itemRequest) {
+        Optional<Category> itemCategory = categoryRepository.findByName(itemRequest.getCategoryName());
+
+        if (itemCategory.isEmpty()) {
+            throw new IllegalArgumentException("Category doesent exist: " + itemRequest.getCategoryName());
+        }
+
+        Item newItem = new Item(itemRequest.getName(), itemCategory.get(), itemRequest.getDescription(), itemRequest.getImageName());
+        return itemRepository.save(newItem);
     }
 }
