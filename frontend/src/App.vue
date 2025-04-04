bd220cae1c5d1cc4186ca2e094c66d8ef67c9048
Ole-Kristian <olemofo@gmail.com>
2025-04-04 13:12:30 +0200

    fixed this trash template

--- frontend/src/App.vue
+++ frontend/src/App.vue
@@ -4,6 +4,13 @@ import HelloWorld from './components/HelloWorld.vue'
 </script>
 
 <template>
+  <header>
+    <nav>
+      <RouterLink to="/">Home</RouterLink>
+      <RouterLink to="/about">Catagory</RouterLink>
+      <RouterLink to="/contact">Search</RouterLink>
+    </nav>
+  </header>
   <main>
     <RouterView />
   </main>
