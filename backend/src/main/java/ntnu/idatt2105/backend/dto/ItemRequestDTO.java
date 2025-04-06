10e5a9c1f15e990bdecf5aaea1e5cdac182dea6d
Henrik André Gaustd <hengau@live.no>
2025-04-06 15:54:54 +0200

    Change getItemByCategory to  return an ItemRequestDTO

--- backend/src/main/java/ntnu/idatt2105/backend/dto/ItemRequestDTO.java
+++ backend/src/main/java/ntnu/idatt2105/backend/dto/ItemRequestDTO.java
@@ -2,12 +2,8 @@ package ntnu.idatt2105.backend.dto;
 
 import com.fasterxml.jackson.annotation.JsonProperty;
 
-import ntnu.idatt2105.backend.model.Item;
-
-/**
- * Data Transfer Object (DTO) for items.
- */
-public class ItemDTO {
+public class ItemRequestDTO {
+    private long id;
     private String name;
     private double price;
     private String description;
@@ -15,16 +11,17 @@ public class ItemDTO {
     private String imageName;
 
     /**
-     * Constructs an ItemDTO object.
+     * Constructs an ItemRequestDTO object.
      * 
-     * @param name
-     * @param price
-     * @param description
-     * @param categoryName
-     * @param imageName
+     * @param id the id of the item
+     * @param name the name of the item
+     * @param description a description of the item
+     * @param categoryName the name of the items category
+     * @param imageName the filename of an image of the item
      */
-    public ItemDTO(@JsonProperty("item_name") String name, @JsonProperty("price") double price, @JsonProperty("description") String description,
+    public ItemRequestDTO(@JsonProperty("item_id") long id, @JsonProperty("item_name") String name, @JsonProperty("price") double price, @JsonProperty("description") String description,
      @JsonProperty("category") String categoryName, @JsonProperty("imageURL") String imageName) {
+        this.id = id;
         this.name = name;
         this.price = price;
         this.description = description;
@@ -33,6 +30,16 @@ public class ItemDTO {
     }
 
     /**
+     * Returns the items ID.
+     * 
+     * @return the items ID
+     */
+    @JsonProperty("item_id")
+    public long getId() {
+        return id;
+    }
+
+        /**
      * Returns the name of the item.
      * 
      * @return the name of the item
@@ -77,7 +84,7 @@ public class ItemDTO {
      * 
      * @return the filename of the image of the item.
      */
-    @JsonProperty("imageURL")
+    @JsonProperty("image_URL")
     public String getImageName() {
         return imageName;
     }
