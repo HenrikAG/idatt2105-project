2510e572f2978762afa2bdc5dff84b7e43d8b239
Henrik André Gaustad <hengau@live.no>
2025-04-07 15:08:06 +0200

    Setup security filter chain

--- backend/src/main/java/ntnu/idatt2105/backend/security/SecurityConfig.java
+++ backend/src/main/java/ntnu/idatt2105/backend/security/SecurityConfig.java
@@ -2,20 +2,38 @@ package ntnu.idatt2105.backend.security;
 
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.Customizer;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.http.SessionCreationPolicy;
 import org.springframework.security.web.SecurityFilterChain;
+import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter;
 
+/**
+ * Configuration class responsible for setting up the security configuration for the applications. 
+ */
 @Configuration
 public class SecurityConfig {
-    //TODO Configure
+
+    private static final String[] AUTH_WHITELIST = {};
+
+    /**
+     * Configures the security filter chain.
+     * 
+     * @param http the HttpSecurity object to configure
+     * @return the SecurityFilterChain instance
+     * @throws Exception if an error occurs during configuration
+     */
     @Bean
-    public SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
+    public SecurityFilterChain filterChain(HttpSecurity http) throws Exception {
         http
+            .cors(Customizer.withDefaults())
+            .csrf(csrf -> csrf.disable())
             .authorizeHttpRequests(auth -> auth
-                .anyRequest().permitAll()
+                .requestMatchers(AUTH_WHITELIST).permitAll()
+                .anyRequest().authenticated()
             )
-            .csrf(crsf -> crsf.disable())
-            .headers(headers -> headers.frameOptions().disable());
+            .sessionManagement(session -> session.sessionCreationPolicy(SessionCreationPolicy.STATELESS))
+            .addFilterBefore(new JWTAuthorizationFilter(), UsernamePasswordAuthenticationFilter.class);
 
         return http.build();
     }
