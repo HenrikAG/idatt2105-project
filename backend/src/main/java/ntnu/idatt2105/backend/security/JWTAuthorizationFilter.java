1aa2828b8c2b518bf6a197b75d6216d0faaef8d0
Henrik André Gaustd <hengau@live.no>
2025-04-08 14:52:54 +0200

    Setup jwt filter

--- backend/src/main/java/ntnu/idatt2105/backend/security/JWTAuthorizationFilter.java
+++ backend/src/main/java/ntnu/idatt2105/backend/security/JWTAuthorizationFilter.java
@@ -2,6 +2,10 @@ package ntnu.idatt2105.backend.security;
 
 import java.io.IOException;
 
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpHeaders;
+import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
+import org.springframework.security.core.context.SecurityContextHolder;
 import org.springframework.web.filter.OncePerRequestFilter;
 
 import jakarta.servlet.FilterChain;
@@ -11,11 +15,46 @@ import jakarta.servlet.http.HttpServletResponse;
 
 public class JWTAuthorizationFilter extends OncePerRequestFilter {
 
+    private final JWTService jwtService;
+    private final CustomUserDetailsService userDetailsService;
+
+    @Autowired
+    public JWTAuthorizationFilter(JWTService jwtService, CustomUserDetailsService userDetailsService) {
+        this.jwtService = jwtService;
+        this.userDetailsService = userDetailsService;
+    }
+
     @Override
     protected void doFilterInternal(HttpServletRequest request, HttpServletResponse response, FilterChain filterChain)
             throws ServletException, IOException {
-        // TODO Auto-generated method stub
-        throw new UnsupportedOperationException("Unimplemented method 'doFilterInternal'");
+                
+        String header = request.getHeader(HttpHeaders.AUTHORIZATION);
+
+        //Request not trying to authenticate.
+        if (header == null || !header.startsWith("Bearer ")) {
+            filterChain.doFilter(request, response);
+            return;
+        }
+
+        String token = header.substring(7);
+        String username = jwtService.validateTokenAndGetUserName(token);
+
+        //Username is null if token not verified.
+        if (username == null) {
+            filterChain.doFilter(request, response);
+            return;
+        }
+
+        CustomUserDetails userDetails = (CustomUserDetails) userDetailsService.loadUserByUsername(username);
+
+        UsernamePasswordAuthenticationToken auth = new UsernamePasswordAuthenticationToken(
+            userDetails,
+            null,
+            userDetails.getAuthorities()
+        );
+
+        SecurityContextHolder.getContext().setAuthentication(auth);
+        filterChain.doFilter(request, response);
     }
     
 }
--- backend/src/main/java/ntnu/idatt2105/backend/security/JWTService.java
+++ backend/src/main/java/ntnu/idatt2105/backend/security/JWTService.java
@@ -3,14 +3,20 @@ package ntnu.idatt2105.backend.security;
 import java.time.Duration;
 import java.time.Instant;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.security.core.Authentication;
 import org.springframework.stereotype.Service;
 
 import com.auth0.jwt.JWT;
+import com.auth0.jwt.JWTVerifier;
 import com.auth0.jwt.algorithms.Algorithm;
+import com.auth0.jwt.exceptions.JWTVerificationException;
 
 @Service
 public class JWTService {
+    private static final Logger logger = LoggerFactory.getLogger(JWTService.class);
+
     private final static Duration JWT_TOKEN_VALIDITY = Duration.ofMinutes(40);
     public final static String tempKey = "my-secret-key"; //TODO ikke ha direkte i koden.
 
@@ -26,5 +32,17 @@ public class JWTService {
             .sign(hmac512);
 
             return token;
+    }
+
+    public String validateTokenAndGetUserName(String token) {
+                try {
+            final Algorithm hmac512 = Algorithm.HMAC512(tempKey);;
+            final JWTVerifier verifier = JWT.require(hmac512)
+                .build();
+            return verifier.verify(token).getSubject();
+        } catch (final JWTVerificationException verificationEx) {
+            logger.warn("token is invalid: {}", verificationEx.getMessage());
+            return null;
+        }
     }
 }
