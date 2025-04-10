d4dec8bc9108aba4b927818b9299a4aa396576a1
Henrik André Gaustad <hengau@live.no>
2025-04-10 15:21:29 +0200

    Fix so user listing are removed on user deletion

--- backend/src/main/java/ntnu/idatt2105/backend/model/User.java
+++ backend/src/main/java/ntnu/idatt2105/backend/model/User.java
@@ -3,6 +3,8 @@ package ntnu.idatt2105.backend.model;
 import java.util.HashSet;
 import java.util.Set;
 
+import org.hibernate.annotations.Cascade;
+
 import jakarta.persistence.CascadeType;
 import jakarta.persistence.Column;
 import jakarta.persistence.Entity;
@@ -35,7 +37,7 @@ public class User {
     @Column(name = "password", nullable = false)
     private String password;
 
-    @OneToMany(mappedBy = "seller")
+    @OneToMany(mappedBy = "seller", cascade = CascadeType.ALL, orphanRemoval = true)
     private Set<Item> listedItems = new HashSet<>();
 
     @ManyToMany
